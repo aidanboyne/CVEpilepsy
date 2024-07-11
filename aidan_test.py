4eca42e0d8da81d483dd7116fd4413f479c69f2c
Aidan Boyne <76006839+aidanboyne@users.noreply.github.com>
2024-07-11 11:29:17 -0500

    Add figures for TEST run to find_optimal_metrics.py

--- aidan_test.py
+++ aidan_test.py
@@ -5,30 +5,34 @@ import argparse
 # Change checkpoint here
 checkpoint_to_test = "C:/Users/u251245/CVEpilepsy_remote/4fold_results_cropped/fold_1/best_top1_acc_epoch_15.pth"
 
-# Example command: python aidan_test.py C:/Users/u251245/CVEpilepsy_remote/4fold_results_cropped/fold_1/best_top1_acc_epoch_15.pth --out test_results_dir --eval save_diagnostics
+# Example command: python aidan_test.py C:/Users/u251245/CVEpilepsy_remote/4fold_results_cropped/fold_1/best_top1_acc_epoch_15.pth
 
 def unix_to_windows_path(path):
     return path.replace('\\', '/')
 
-def main(checkpoint_to_test=checkpoint_to_test, work_dir=os.path.join(os.getcwd(), "./work_dir_test"), output_dir=os.path.join(os.getcwd(), "./output_dir_test")):
-    cv_dir = os.path.join(os.getcwd(), "annotations", "4fold_CV")
+def main(config, checkpoint_to_test, work_dir, output_dir):
+    video_dir = os.path.join(os.getcwd(), "annotations", "video_test_annotations")
+    annotation_files = []
+    patients = []
+    for dirpath, dirnames, filenames in os.walk(video_dir):
+        for filename in filenames:
+            patients.append(filename.strip(".txt"))
+            annotation_files.append(unix_to_windows_path(os.path.join(dirpath, filename)))
     
-    for fold in range(1, 5):  # 4 folds
-        fold_dir = os.path.join(cv_dir, f"fold_{fold}")
-        fold_output_dir = os.path.join(output_dir, f"fold_{fold}")
+    for i, annotation in enumerate(annotation_files):
+        test_ann = annotation
+        fold_output_dir = os.path.join(output_dir, f'video_{patients[i]}')
         os.makedirs(fold_output_dir, exist_ok=True)
 
-        test_ann = os.path.join(fold_dir, "test.txt")
-
         # Create a temporary config file for this fold
-        temp_config = os.path.join(fold_output_dir, f"i3d_config_fold_{fold}.py")
+        temp_config = os.path.join(fold_output_dir, f"i3d_config_{patients[i]}.py")
         
         with open(temp_config, 'w') as f:
             f.write(f"""
-                    base_path = "C:/Users/u251245/CVEpilepsy/src/mmaction2/configs/_base_/"
+base_path = "C:/Users/u251245/CVEpilepsy/src/mmaction2/configs/_base_/"
 data_path = "C:/Users/u251245/CVEpilepsy/video_clips_cropped/"
 model_path = "{checkpoint_to_test}"
-checkpoint_path = "{output_dir}"
+checkpoint_path = "{unix_to_windows_path(output_dir)}"
 
 # Model Parameters
 eval_metrics = ['top_k_accuracy', 'aidan_acc', 'aidan_auc', 'modified_acc', 'modified_auc', 'save_diagnostics']
@@ -101,23 +105,26 @@ gpu_ids = range(1)
             """)
 
         # Run the test
-        raw_output_file = os.path.join(fold_output_dir, "raw_output.txt")
-        command = f"python tools/test.py {temp_config} {checkpoint_to_test} --work-dir {work_dir} --out {raw_output_file}"
+        raw_output_file = os.path.join(fold_output_dir, "raw_output.json")
+        command = f"python tools/test.py {temp_config} {checkpoint_to_test} --out {raw_output_file} --eval save_diagnostics" 
         
         try:
             subprocess.run(command, check=True, shell=True)
-            print(f"\nTesting succeeded for fold {fold}. Output saved to {fold_output_dir}\n")
+            print(f"\nTesting succeeded for video {patients[i]}. Output saved to {fold_output_dir}\n")
         except subprocess.CalledProcessError as e:
-            print(f"Testing failed for fold {fold} with error: {e}")
+            print(f"Testing failed for fold {patients[i]} with error: {e}")
             continue
 
     print("\n\nTesting for all folds completed. Exiting...")
 
 if __name__ == "__main__":
+    cwd = os.getcwd()
+    work_default = os.path.join(cwd, "test_work_dir")
+    output_default = os.path.join(cwd, "test_output_dir")
     parser = argparse.ArgumentParser(description="Test MMAction2 models for 4-fold cross-validation.")
-    parser.add_argument('checkpoint', help='Path to the checkpoint file')
-    parser.add_argument('--work-dir', default='./work_dirs', help='Directory where testing outputs are saved')
-    parser.add_argument('--output-dir', default='./test_output', help='Directory where raw outputs are saved')
-    
+    parser.add_argument('checkpoint', default=checkpoint_to_test, help='Path to the checkpoint file')
+    parser.add_argument('--work-dir', default=work_default, help='Directory where testing outputs are saved')
+    parser.add_argument('--output-dir', default=output_default, help='Directory where raw outputs are saved')
+    config = ""
     args = parser.parse_args()
-    main(args.config, args.checkpoint, args.work_dir, args.output_dir)
+    main(config, args.checkpoint, args.work_dir, args.output_dir)
